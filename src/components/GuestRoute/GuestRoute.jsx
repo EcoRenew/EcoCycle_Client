efba7ec6eb8cb944e2795dc456ce43dd767d7f86
K-nass <karimnase10@gmail.com>
2025-09-19 02:14:24 +0300

    added guest route

--- src/components/GuestRoute/GuestRoute.jsx
+++ src/components/GuestRoute/GuestRoute.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 
-export default function ProtectedRoute({ children }) {
+export default function GuestRoute({ children }) {
   const { user } = useAuth();
 
-  if (!user) {
-    return <Navigate to="/login" replace />;
+  if (user) {
+    return <Navigate to="/" replace />;
   }
 
   return children;
