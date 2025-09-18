98e5a9d7ae1e663918237716b9f51e3101f36ac9
K-nass <karimnase10@gmail.com>
2025-09-19 02:39:07 +0300

    resolve conflict

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 
@@ -12,14 +11,4 @@ export default function ProtectedRoute({ children }) {
 
   return children;
 }
-=======
-import { useAuth } from "../../context/AuthContext";
 
-export default function ProtectedRoute({children}) {
-    const {user} = useAuth();
-    if (!user) {
-      return <Navigate to="/login" replace />;
-    }
-    return children;
-}
->>>>>>> 86a96c8 (added logout and protected route (#32))
